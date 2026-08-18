940529509de2caf6152222f26d5ff4ec187df070
hehe-lty <1570668005@qq.com>
2026-08-18 23:53:11 +0800

    6.4

--- chapt6/chapt6_ws/src/fishbot_description/launch/gazebo_sim.launch.py
+++ chapt6/chapt6_ws/src/fishbot_description/launch/gazebo_sim.launch.py
@@ -56,6 +56,22 @@ def generate_launch_description():
     #     arguments=['-d', default_rviz_config_path]
     # )
 
+    # 加载并激活 fishbot_joint_state_broadcaster 控制器
+    load_joint_state_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+            'fishbot_joint_state_broadcaster'],
+        output='screen'
+    )
+
+    # 加载并激活 fishbot_effort_controller 控制器
+    load_fishbot_effort_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active','fishbot_effort_controller'], 
+        output='screen')
+    
+    load_fishbot_diff_drive_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active','fishbot_diff_drive_controller'], 
+        output='screen')
+
     return launch.LaunchDescription([
         action_declare_arg_urdf_path,
         action_robot_state_publisher,
@@ -63,4 +79,16 @@ def generate_launch_description():
         spawn_entity_node,
         # action_joint_state_publisher,
         # action_rviz2_node,
+        # 事件动作，当加载机器人结束后执行    
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=spawn_entity_node,
+                on_exit=[load_joint_state_controller],)
+            ),
+        # 事件动作，load_fishbot_diff_drive_controller
+        launch.actions.RegisterEventHandler(
+        event_handler=launch.event_handlers.OnProcessExit(
+            target_action=load_joint_state_controller,
+            on_exit=[load_fishbot_diff_drive_controller],)
+            ),
     ])
